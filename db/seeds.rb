e76a77efc62a9a565cdaaca704361b58adb90c36
James Coffman <jamesc4023@gmail.com>
2022-05-20 09:56:50 -0400

    commit tree 9aebf5cb1baeeda5ea26f1fd69a39392f05af89b

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,26 @@
-# Add seed data in this file
+puts 'Seeding spells...'
+
+#arr of spells we want to add to the db
+spells = ['acid-arrow', 'animal-messenger', 'calm-emotions', 'charm-person']
+
+#iterate over each spell
+spells.each do |spell|
+  
+  #make a request to the endpoint for the individual spell:
+  res = RestClient.get "https://www.dnd5eapi.co/api/spells/#{spell}"
+
+  #the res will come back as a JSON-formatted string.
+  #JSON.parse converts this string to a ruby hash:
+  spell_hash = JSON.parse(res)
+
+  #create a spell in the db using the data from the spell_hash
+
+  Spell.create(
+    name: spell_hash['name'],
+    level: spell_hash['level'],
+    description: spell_hash['desc'][0]
+  )
+
+end
+
+puts 'Done seeding!'
